50a9b8d500fb3e3857df7bb86c7c986e824eaa84
CleuJunior <cleu.junior@gmail.com>
2021-09-15 01:11:25 -0300

    Backend project created

---
+++ src/main/java/com/devsuperior/dslearnbds/DslearnbdsApplication.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dslearnbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnbdsApplication.class, args);
+	}
+}
