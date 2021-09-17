376e979986e88c007060ea8b6610fb4db3d4a7e6
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2021-09-16 21:56:36 -0300

    Lesson, Content, Task

--- src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
+++ src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -1,2 +1,104 @@
-package com.devsuperior.dslearnbds.entities;public class Enrollment {
+package com.devsuperior.dslearnbds.entities;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
+import com.devsuperior.dslearnbds.entities.User;
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment {
+
+    @EmbeddedId
+    private EnrollmentPK id = new EnrollmentPK();
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant enrollMoment;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant refundMoment;
+
+    private boolean available;
+    private boolean onlyUpdate;
+
+//    @ManyToMany(mappedBy = "enrollmentsDone")
+//    private Set<Lesson> lessonsDone = new HashSet<>();
+//
+//    @OneToMany(mappedBy = "enrollment")
+//    private List<Deliver> deliveries = new ArrayList<>();
+
+    public Enrollment() { }
+
+    public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refundMoment, boolean available,
+                      boolean onlyUpdate) {
+        super();
+        id.setUser(user);
+        id.setOffer(offer);
+        this.enrollMoment = enrollMoment;
+        this.refundMoment = refundMoment;
+        this.available = available;
+        this.onlyUpdate = onlyUpdate;
+    }
+
+    public User getStudent() { return id.getUser(); }
+
+    public void setStudent(User user) { id.setUser(user); }
+
+    public Offer getOffer() {  return id.getOffer(); }
+
+    public void setOffer(Offer offer) { id.setOffer(offer); }
+
+    public Instant getEnrollMoment() { return enrollMoment; }
+
+    public void setEnrollMoment(Instant enrollMoment) { this.enrollMoment = enrollMoment;  }
+
+    public Instant getRefundMoment() { return refundMoment; }
+
+    public void setRefundMoment(Instant refundMoment) { this.refundMoment = refundMoment; }
+
+    public boolean isAvailable() { return available; }
+
+    public void setAvailable(boolean available) { this.available = available; }
+
+    public boolean isOnlyUpdate() { return onlyUpdate; }
+
+    public void setOnlyUpdate(boolean onlyUpdate) { this.onlyUpdate = onlyUpdate; }
+
+//    public List<Deliver> getDeliveries() { return deliveries; }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Enrollment other = (Enrollment) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -27,3 +27,7 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) 
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar', 1, 'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg', 1, null);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos continuar', 2, 'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg', 1, 1);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capítulo vamos finalizar', 3, 'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg', 1, 2);
+
+
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false);
